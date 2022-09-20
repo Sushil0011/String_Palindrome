7f664c7a499714e7211341694295d001e59d3843
SUshil Tripathi <yashtripathi861@gmail.com>
2022-09-20 12:37:40 +0530

    Code for Plaindrome String

---
+++ palindrome.js
@@ -0,0 +1,18 @@
+function palindrome(str){
+    let reverse_str="";
+    for(let i=str.length-1;i>=0;i--){
+reverse_str+=str[i];
+    }
+
+    if(str===reverse_str){
+        return true;
+    }
+    return false;
+}
+let answer=palindrome("naman");
+if(answer==true){
+    console.log("Palindrome String")
+}
+else{
+    console.log("Not A Palindrome String");
+}
